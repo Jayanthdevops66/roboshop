60021e6a200c81a137e23485cba1eff240213ab2
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-08-27 16:53:41 -0400

    updating ./IdeaProjects/roboshop

---
+++ shipping.sh
@@ -0,0 +1,16 @@
+yuminstallmaven-y
+
+useraddroboshop
+
+cd/home/roboshop
+curl-s-L-o/tmp/shipping.zip"https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+unzip/tmp/shipping.zip
+mvshipping-mainshipping
+cdshipping
+mvncleanpackage
+mvtarget/shipping-1.0.jarshipping.jar
+
+mv/home/roboshop/shipping/systemd.service/etc/systemd/system/shipping.service
+systemctldaemon-reload
+systemctlstartshipping
+systemctlenableshipping
