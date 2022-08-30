dcffa964d46a0d1769ed0482d1c5f3ec042ba7bd
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-08-30 13:17:53 -0400

    changes

--- cart.sh
+++ cart.sh
@@ -1,31 +1,76 @@
 echo Setting nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Installing nodejs
 yum install nodejs -y &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Adding Application user
 useradd roboshop &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Downloading application content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
 cd /home/roboshop &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Cleaning old Application Content
 rm -rf cart &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Extract Appplication Content
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Installing Nodejs Dependencies
 npm install &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Configuring cart SystemD Service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo Starting Cart Service
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
