6842087c874bffe54ecbea9cd3201e50ede59bea
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-08-30 13:47:03 -0400

    changes

--- cart.sh
+++ cart.sh
@@ -14,13 +14,15 @@ else
   echo -e "\e[31mSUCCESS\e[0m"
 fi
 
-echo Adding Application user
-useradd roboshop &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-fi
+id roboshop &>>/tmp/cart.log
+if [ $? -ne 0 ]; then
+  echo Adding Application user
+  useradd roboshop &>>/tmp/cart.log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mSUCCESS\e[0m"
+  fi
 
 echo Downloading application content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
