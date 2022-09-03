e8ae0bbf16dc850ad454b8745dc5bb9e0881bea6
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-09-03 19:43:31 -0400

    changes

--- cart.sh
+++ cart.sh
@@ -1,90 +1,53 @@
-
-echo Setting nodejs repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
-
-echo Installing nodejs
-yum install nodejs -y &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
-
-id roboshop &>>/tmp/cart.log
-if [ $? -ne 0 ]; then
-  echo Adding Application user
-  useradd roboshop &>>/tmp/cart.log
+StatusCheck() {
   if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mSUCCESS\e[0m"
     exit 1
   fi
+}
+
+echo Setting nodejs repos
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
+StatusCheck
+
+echo Installing nodejs
+yum install nodejs -y &>>/tmp/cart.log
+StatusCheck
+
+id roboshop &>>/tmp/cart.log
+if [ $? -ne 0 ]; then
+  echo Adding Application user
+  useradd roboshop &>>/tmp/cart.log
+  StatusCheck
 fi
 
 
 echo Downloading application content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
 cd /home/roboshop &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Cleaning old Application Content
 rm -rf cart &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Extract Appplication Content
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Installing Nodejs Dependencies
 npm install &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Configuring cart SystemD Service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Starting Cart Service
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 1
-fi
+StatusCheck
