5694638bebe064cde5b4fb1142d9f7b14b1423ae
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-09-03 20:58:08 -0400

    changes

--- common.sh
+++ common.sh
@@ -8,35 +8,35 @@ StatusCheck() {
 }
  NodeJs() {
    echo Setting nodejs repos
-   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/$(COMPONENT).log
+   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/${COMPONENT}.log
    StatusCheck
 
    echo Installing nodejs
-   yum install nodejs -y &>>/tmp/$(COMPONENT).log
+   yum install nodejs -y &>>/tmp/${COMPONENT}.log
    StatusCheck
 
-   id roboshop &>>/tmp/$(COMPONENT).log
+   id roboshop &>>/tmp/${COMPONENT}.log
    if [ $? -ne 0 ]; then
      echo Adding Application user
-     useradd roboshop &>>/tmp/$(COMPONENT).log
+     useradd roboshop &>>/tmp/${COMPONENT}.log
      StatusCheck
    fi
    echo Downloading application content
-   curl -s -L -o /tmp/$(COMPONENT).zip "https://github.com/roboshop-devops-project/$(COMPONENT)/archive/main.zip" &>>/tmp/$(COMPONENT).log
-   cd /home/roboshop &>>/tmp/$(COMPONENT).log
+   curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>/tmp/${COMPONENT}.log
+   cd /home/roboshop &>>/tmp/${COMPONENT}.log
    StatusCheck
 
    echo Cleaning old Application Content
-   rm -rf $(COMPONENT) &>>/tmp/$(COMPONENT).log
+   rm -rf ${COMPONENT} &>>/tmp/${COMPONENT}.log
    StatusCheck
 
    echo Extract Appplication Content
-   unzip -o /tmp/$(COMPONENT).zip &>>/tmp/$(COMPONENT).log
-   mv $(COMPONENT)-main $(COMPONENT) &>>/tmp/$(COMPONENT).log
-   cd $(COMPONENT) &>>/tmp/$(COMPONENT).log
+   unzip -o /tmp/${COMPONENT}.zip &>>/tmp/${COMPONENT}.log
+   mv ${COMPONENT}-main ${COMPONENT} &>>/tmp/${COMPONENT}.log
+   cd ${COMPONENT} &>>/tmp/${COMPONENT}.log
    StatusCheck
 
    echo Installing Nodejs Dependencies
-   npm install &>>/tmp/$(COMPONENT).log
+   npm install &>>/tmp/${COMPONENT}.log
    StatusCheck
  }
