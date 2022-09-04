d943b6d9c228a74ac15e3c037809208ffe3c89fb
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-09-03 20:41:41 -0400

    changes

--- cart.sh
+++ cart.sh
@@ -2,26 +2,7 @@ source common.sh
 
 
 Nodejs
-
-
-echo Downloading application content
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
-cd /home/roboshop &>>/tmp/cart.log
-StatusCheck
-
-echo Cleaning old Application Content
-rm -rf cart &>>/tmp/cart.log
-StatusCheck
-
-echo Extract Appplication Content
-unzip -o /tmp/cart.zip &>>/tmp/cart.log
-mv cart-main cart &>>/tmp/cart.log
-cd cart &>>/tmp/cart.log
-StatusCheck
-
-echo Installing Nodejs Dependencies
-npm install &>>/tmp/cart.log
-StatusCheck
+COMPONENT=cart
 
 echo Configuring cart SystemD Service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
