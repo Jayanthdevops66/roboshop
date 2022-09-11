79eb79dd7c4299ca28cbcf9b06c712858e5fb31e
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-09-11 18:49:28 -0400

    changes

--- mysql.sh
+++ mysql.sh
@@ -13,11 +13,11 @@ echo Start MySql service
 systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 
 DEFAULT_PASSWORD=$(grep "A tem" /var/log/mysqld.log | awk '{print $NF}')
-echo "alter use 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql -uroot -p${DEFAULT_PASSWORD}
+echo "alter use 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql -uroot -p${DEFAULT_PASSWORD}
 
 mysql_secure_installation
 
-mysql -uroot -pRoboShop@1
+echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
 #> uninstall plugin validate_password;
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
