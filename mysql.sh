3aa312c5154f30bb86e44c1c3695dcb14603ebdb
jayanthreddychenannagaari <reddy.jayanthc@gmail.com>
2022-09-11 20:16:50 -0400

    changes

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,11 @@
 source common.sh
 COMPONENT=mysql
 
+if [ -z "MYSQL_PASSWORD" ];
+  echo -e "\e[33m env variable MYSQL_PASSWORD is missing \e[0m"
+  exit 1
+fi
+
 echo Setup Yum Repos
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
 StatusCheck
@@ -11,9 +16,15 @@ StatusCheck
 
 echo Start MySql service
 systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+StatusCheck
 
-DEFAULT_PASSWORD=$(grep "A tem" /var/log/mysqld.log | awk '{print $NF}')
-echo "alter use 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql -uroot -p${DEFAULT_PASSWORD}
+echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+if [ $? -ne 0 ]; then
+  echo Changing Default Password
+  DEFAULT_PASSWORD=$(grep "A tem" /var/log/mysqld.log | awk '{print $NF}')
+  echo "alter use 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql -uroot -p${DEFAULT_PASSWORD}
+  StatusCheck
+fi
 
 mysql_secure_installation
 
